fd67a8d892c0f36c6957ec8005692efe14fa3349
KusalDemo <kusalgunasekara2002@gmail.com>
2024-11-15 02:23:11 +0530

    Implemented Equipment update , delete clear Equipment form fields

--- controller/EquipmentController.js
+++ controller/EquipmentController.js
@@ -12,6 +12,21 @@ document.addEventListener("DOMContentLoaded", () => {
     if (btnEquipmentSave) {
         btnEquipmentSave.addEventListener('click', () => saveEquipment(jwtToken));
     }
+
+    let btnEquipmentUpdate = document.getElementById("equipment-update-btn");
+    if (btnEquipmentUpdate) {
+        btnEquipmentUpdate.addEventListener('click', () => updateEquipment(jwtToken));
+    }
+
+    let btnClearEquipmentForm = document.getElementById("equipment-clear-btn");
+    if (btnClearEquipmentForm) {
+        btnClearEquipmentForm.addEventListener('click', () => clearEquipmentForm());
+    }
+
+    let btnDeleteEquipment = document.getElementById("equipment-delete-btn");
+    if (btnDeleteEquipment) {
+        btnDeleteEquipment.addEventListener('click', () => deleteEquipment(jwtToken));
+    }
 })
 
 const loadTable = (jwtToken) => {
@@ -50,7 +65,7 @@ const loadTable = (jwtToken) => {
                 row.addEventListener("click", () => updateFormFields(equipment));
                 equipmentTable.appendChild(row);
             })
-            new DataTable('#equipment-table',{paging: true, pageLength: 10, destroy: true});
+            new DataTable('#equipment-table', {paging: true, pageLength: 10, destroy: true});
         },
         error: (error) => {
             console.error(error);
@@ -69,28 +84,28 @@ const loadStaffs = (jwtToken) => {
     if (!equipmentStaffSelector) return;
 
     $.ajax({
-      url : "http://localhost:8082/api/v1/staff",
-      method : "GET",
-      headers: {
-        "Authorization": `Bearer ${jwtToken}`
-      },
-      success : (data) => {
-        data.forEach((staff) => {
-          let option = document.createElement("option");
-            option.innerHTML = `${staff.firstName} ${staff.lastName} - ${staff.designation}`;
-            option.value = staff.id;
-          equipmentStaffSelector.appendChild(option);
-        })
-      },
-      error : (error) => {
-        console.error(error);
-        const errorMessage = error.responseText || "An unexpected error occurred.";
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: `${errorMessage}`,
-        })
-      }
+        url: "http://localhost:8082/api/v1/staff",
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${jwtToken}`
+        },
+        success: (data) => {
+            data.forEach((staff) => {
+                let option = document.createElement("option");
+                option.innerHTML = `${staff.firstName} ${staff.lastName} - ${staff.designation}`;
+                option.value = staff.id;
+                equipmentStaffSelector.appendChild(option);
+            })
+        },
+        error: (error) => {
+            console.error(error);
+            const errorMessage = error.responseText || "An unexpected error occurred.";
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `${errorMessage}`,
+            })
+        }
     })
 }
 
@@ -100,59 +115,162 @@ const loadFieldCodes = (jwtToken) => {
     if (!equipmentFieldSelector) return;
 
     $.ajax({
-      url: "http://localhost:8082/api/v1/field",
-      method: "GET",
-      headers: {
-        "Authorization": `Bearer ${jwtToken}`
-      },
-      success: (data) => {
-        data.forEach((field) => {
-          let option = document.createElement("option");
-          option.innerHTML = field.fieldName;
-          option.value = field.fieldCode;
-          equipmentFieldSelector.appendChild(option);
-        })
-      }
+        url: "http://localhost:8082/api/v1/field",
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${jwtToken}`
+        },
+        success: (data) => {
+            data.forEach((field) => {
+                let option = document.createElement("option");
+                option.innerHTML = field.fieldName;
+                option.value = field.fieldCode;
+                equipmentFieldSelector.appendChild(option);
+            })
+        }
     })
 }
 
 
 const saveEquipment = async (jwtToken) => {
-    let equipmentToSave = await getValuesInEquipmentForm();
-    if (!equipmentToSave) return;
 
-    try {
-        $.ajax({
-            url: "http://localhost:8082/api/v1/equipment",
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${jwtToken}`
-            },
-            data: JSON.stringify(equipmentToSave),
-            contentType: "application/json",
-            success: () => {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Success',
-                    text: 'Equipment added successfully',
-                })
-                loadTable(jwtToken);
-            },
-            error: (error) => {
-                const errorMessage = error.responseText || "An unexpected error occurred.";
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: `${errorMessage}`,
-                })
-            }
-        })
-    } catch (error) {
-        console.log(error);
+    if (document.getElementById("equipment-id").innerText !== "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Equipment is already added. Please update the equipment details',
+        });
+    } else {
+        let equipmentToSave = await getValuesInEquipmentForm();
+        if (!equipmentToSave) return;
+
+        try {
+            $.ajax({
+                url: "http://localhost:8082/api/v1/equipment",
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${jwtToken}`
+                },
+                data: JSON.stringify(equipmentToSave),
+                contentType: "application/json",
+                success: () => {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success',
+                        text: 'Equipment added successfully',
+                    })
+                    loadTable(jwtToken);
+                    clearEquipmentForm();
+                },
+                error: (error) => {
+                    const errorMessage = error.responseText || "An unexpected error occurred.";
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: `${errorMessage}`,
+                    })
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
+const updateEquipment = async (jwtToken) => {
+    if (document.getElementById("equipment-id").innerText === "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please select an equipment first',
+        })
+    } else {
+        let equipmentToUpdate = await getValuesInEquipmentForm();
+        if (!equipmentToUpdate) return;
 
+        try {
+            $.ajax({
+                url: "http://localhost:8082/api/v1/equipment/" + equipmentToUpdate.equipmentId,
+                method: "PUT",
+                headers: {
+                    "Authorization": `Bearer ${jwtToken}`
+                },
+                data: JSON.stringify(equipmentToUpdate),
+                contentType: "application/json",
+                success: () => {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success',
+                        text: 'Equipment updated successfully',
+                    })
+                    loadTable(jwtToken);
+                    clearEquipmentForm();
+                },
+                error: (error) => {
+                    const errorMessage = error.responseText || "An unexpected error occurred.";
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: `${errorMessage}`,
+                    })
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+const deleteEquipment = (jwtToken) => {
+    let equipmentToDelete = document.getElementById("equipment-id").innerText;
+    if (equipmentToDelete) {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#0D9F4F',
+            confirmButtonText: 'Yes, Delete!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: "http://localhost:8082/api/v1/equipment/" + document.getElementById("equipment-id").innerText,
+                    method: "DELETE",
+                    headers: {
+                        "Authorization": `Bearer ${jwtToken}`
+                    },
+                    success: () => {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Equipment deleted successfully',
+                        })
+                        loadTable(jwtToken);
+                        clearEquipmentForm();
+                    },
+                    error: (error) => {
+                        const errorMessage = error.responseText || "An unexpected error occurred.";
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: `${errorMessage}`,
+                        })
+                    }
+                })
+            }
+        })
+    }
+}
+
+const clearEquipmentForm = () => {
+    document.getElementById("equipment-id").innerHTML = "";
+    document.getElementById("equipment-name-input").value = "";
+    document.getElementById("equipment-type-select").value = "";
+    document.getElementById("equipment-status-select").value = "";
+    document.getElementById("equipment-staff-select").value = "";
+    document.getElementById("equipment-field-select").value = "";
+}
 
 
 const getCookie = (name) => {
@@ -162,12 +280,12 @@ const getCookie = (name) => {
 };
 
 const updateFormFields = (equipment) => {
-    document.getElementById("equipment-id").innerHTML=equipment.equipmentId
+    document.getElementById("equipment-id").innerHTML = equipment.equipmentId
     document.getElementById("equipment-name-input").value = equipment.name;
     document.getElementById("equipment-type-select").value = equipment.type;
     document.getElementById("equipment-status-select").value = equipment.status;
-    document.getElementById("equipment-staff-select").value=equipment.staffId;
-    document.getElementById("equipment-field-select").value=equipment.fieldCode;
+    document.getElementById("equipment-staff-select").value = equipment.staffId;
+    document.getElementById("equipment-field-select").value = equipment.fieldCode;
 }
 const getValuesInEquipmentForm = async () => {
     let equipmentId = document.getElementById("equipment-id").innerText;
@@ -184,7 +302,7 @@ const getValuesInEquipmentForm = async () => {
             text: 'Please fill all the fields',
         })
         return;
-    }else{
+    } else {
         let equipmentModel = new EquipmentModel();
 
         equipmentModel.setEquipmentId(equipmentId);
